19744c4e94d20cf824cd6891814a06521e90c45b
NumLock <205994552+adm1n5ky@users.noreply.github.com>
2026-05-25 14:08:52 +0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+set -e
+
+PKG_NAME="luci-app-dnsproxy"
+PKG_VERSION="${1:-0.1.0}"
+PKG_RELEASE="1"
+PKG_ARCH="all"
+PKG_DEPENDS="luci-base dnsproxy luci-mod-status"
+PKG_DESCRIPTION="LuCI web interface for AdGuard DNS Proxy. Supports DoT, DoH, DoQ, DNSCrypt."
+PKG_MAINTAINER="NumLock"
+PKG_LICENSE="Apache-2.0"
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+BUILD_DIR="${SCRIPT_DIR}/.build"
+PKG_DIR="${BUILD_DIR}/pkg"
+OUT_DIR="${SCRIPT_DIR}/dist"
+
+echo "==> Building ${PKG_NAME}-${PKG_VERSION}-r${PKG_RELEASE}-${PKG_ARCH}.apk"
+
+# Чистим предыдущую сборку
+rm -rf "${BUILD_DIR}"
+mkdir -p "${PKG_DIR}"
+mkdir -p "${OUT_DIR}"
+
+# Копируем файлы пакета
+cp -r "${SCRIPT_DIR}/usr" "${PKG_DIR}/"
+cp -r "${SCRIPT_DIR}/www" "${PKG_DIR}/"
+
+# Считаем размер
+PKG_SIZE=$(du -sk "${PKG_DIR}" | cut -f1)
+
+# Создаём APKBUILD-совместимый .PKGINFO
+cat > "${BUILD_DIR}/.PKGINFO" << PKGINFO
+pkgname = ${PKG_NAME}
+pkgver = ${PKG_VERSION}-r${PKG_RELEASE}
+arch = ${PKG_ARCH}
+size = ${PKG_SIZE}
+pkgdesc = ${PKG_DESCRIPTION}
+url = https://github.com/adm1n5ky/${PKG_NAME}
+builddate = $(date +%s)
+packager = ${PKG_MAINTAINER}
+license = ${PKG_LICENSE}
+depend = luci-base
+depend = dnsproxy
+depend = luci-mod-status
+PKGINFO
+
+# Создаём tar с файлами пакета
+cd "${PKG_DIR}"
+tar -czf "${BUILD_DIR}/data.tar.gz" usr/ www/
+
+# Создаём финальный apk (это tar.gz из .PKGINFO + data.tar.gz)
+cd "${BUILD_DIR}"
+tar -czf "${OUT_DIR}/${PKG_NAME}-${PKG_VERSION}-r${PKG_RELEASE}-${PKG_ARCH}.apk" \
+    .PKGINFO \
+    data.tar.gz
+
+echo "==> Done: dist/${PKG_NAME}-${PKG_VERSION}-r${PKG_RELEASE}-${PKG_ARCH}.apk"
